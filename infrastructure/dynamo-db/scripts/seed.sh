81ea1122714847ac9b97c05ef964fde05b2ef8c9
tyler mccracken <tylermccracken@tylers-MacBook-Pro.local>
2025-03-22 23:22:30 -0400

    add stage environment to seed script

--- infrastructure/dynamo-db/scripts/seed.sh
+++ infrastructure/dynamo-db/scripts/seed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuration
-TABLE_NAME="sit-MapKrakenContentTable"
+TABLE_NAME="$1-MapKrakenContentTable"
 REGION="us-east-1"
 
 # Function to add or update an item in DynamoDB
@@ -14,6 +14,12 @@ update_item() {
   local description="$6"
   local resources="$7"
 
+  echo "Updating item with PK: $pk, SK: $sk in $TABLE_NAME..."
+  echo "Site URL: $site_url"
+  echo "Repo URLs: $repo_urls"
+  echo "Title: $title"
+  echo "Description: $description"
+  echo "Resources: $resources"
   aws dynamodb update-item \
     --table-name "$TABLE_NAME" \
     --key '{
@@ -32,11 +38,18 @@ update_item() {
     --return-consumed-capacity TOTAL
 }
 
+# Determine the site_url based on the environment
+if [ "$1" == "prd" ]; then
+  SITE_URL="https://terriamap.mapkraken.com"
+else
+  SITE_URL="https://$1-terriamap.mapkraken.com"
+fi
+
 # Seed portfolio items
 echo "Seeding portfolio data into $TABLE_NAME..."
 
 update_item "PORTFOLIO#1" "#ITEM1" \
-  "https://sit-terriamap.mapkraken.com" \
+  "$SITE_URL" \
   '{"S": "https://github.com/mapkraken/mapkraken-portal"}' \
   "Terriamap" \
   "Containerization and customization of a terriamap application" \
